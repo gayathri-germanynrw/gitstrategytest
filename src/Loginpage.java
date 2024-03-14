1d8e21e71a90142fced04249eeb595b27c6906dc
gayat <gayathrikasi@gmail.com>
2024-03-14 09:02:53 +0100

    login page added

--- src/Loginpage.java
+++ src/Loginpage.java
@@ -1,5 +1,7 @@
 public class Loginpage {
     public static void main(String[] args) {
         System.out.println(" hello world");
+        System.out.println("cydeo school is the best ");
+        System.out.println("we are learning intensively");
     }
 }
