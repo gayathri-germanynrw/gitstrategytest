fd5598cc69d1b5b4644e4c547fa85ca7820989a0
gayat <gayathrikasi@gmail.com>
2024-03-14 09:03:56 +0100

    login page added

--- src/Loginpage.java
+++ src/Loginpage.java
@@ -1,5 +1,6 @@
 public class Loginpage {
     public static void main(String[] args) {
         System.out.println(" hello world");
+        System.out.println("SDET Engineer is responsible for good quality projects");
     }
 }
